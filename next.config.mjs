af0b3220e7a15c315dbd92cfd5f491e9fcc61875
starjefferson <jeffersonokoene24@gmail.com>
2025-12-14 19:46:00 +0100

    Remove unsupported turbotrace option

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    turbotrace: { sourceMap: false },
     serverSourceMaps: false,
   },
   productionBrowserSourceMaps: false,
