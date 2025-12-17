1444fdf9a9f82669bf28f403e422b8a7f1a9e14f
starjefferson <jeffersonokoene24@gmail.com>
2025-12-17 17:10:54 +0100

    responsive setting

--- app/api/movies/route.js
+++ app/api/movies/route.js
@@ -2,6 +2,11 @@ import { getPopularMovies } from "@/lib/api";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+  if (!API_KEY) {
+    console.error("TMDB API key is missing on server");
+    return NextResponse.json({ error: "API key missing" }, { status: 500 });
+  }
   try {
     const movies = await getPopularMovies(10);
     return NextResponse.json(movies);
